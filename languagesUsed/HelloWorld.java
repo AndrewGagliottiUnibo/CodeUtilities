a5ca8ced1e500e737f05f09223083357c1e4b994
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-11-04 17:15:02 +0100

    dummy files

---
+++ languagesUsed/HelloWorld.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Esempio di prima applicazione
+ */
+class HelloWorld {
+
+    public static void main(final String[] argv) {
+        System.out.println("Java is better!");
+    }
+}
